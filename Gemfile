8b4a0a4034f5a9ee392aba9a97bba9006155f6f4
Al Jade Palaran <aljadepalaran@Als-MacBook-Air.local>
2025-05-27 20:38:42 +0100

    add pagination

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 gem "solid_cache"
 gem "solid_queue"
 gem "solid_cable"
+gem "kaminari"
 gem "bootsnap", require: false
 gem "kamal", require: false
 gem "thruster", require: false
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,16 @@
 class Api::UsersController < ApplicationController
   def index
-    @users = User.select(params[:fields]).all
     @fields = params[:fields] || User.column_names
+    @users = User.select(@fields).page(page).per(per_page)
+  end
+
+  private
+
+  def per_page
+    params[:per_page] || 20
+  end
+
+  def page
+    params[:page] || 1
   end
 end
